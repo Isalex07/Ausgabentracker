ae00e700144510483c2214bde777711a929f4132
Isalex07 <91074885+Isalex07@users.noreply.github.com>
2023-02-28 16:31:01 +0100

    Update testausgabentracker.sql

--- testausgabentracker.sql
+++ testausgabentracker.sql
@@ -1,27 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Erstellungszeit: 28. Feb 2023 um 16:28
--- Server-Version: 10.4.27-MariaDB
--- PHP-Version: 8.1.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Datenbank: `testausgabentracker`
 --
 
--- --------------------------------------------------------
 
 --
 -- Tabellenstruktur für Tabelle `ausgabe`
@@ -275,6 +262,3 @@ ALTER TABLE `protokoll`
   ADD CONSTRAINT `protokoll_ibfk_1` FOREIGN KEY (`AusgabenID`) REFERENCES `ausgabe` (`AusgabeID`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
